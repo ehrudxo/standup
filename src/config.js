ca8035afc147fd3720ab27728f24b8f9d4ea09b1
keen <ehrudxo@hotmail.com>
2016-10-31 06:40:26 +0900

    Card added

---
+++ src/config.js
@@ -0,0 +1,8 @@
+var config = {
+ apiKey: process.env.REACT_APP_FIREBASE_KEY,
+ databaseURL: process.env.REACT_APP_DB_URL,
+ storageBucket: process.env.REACT_APP_STRG_BKT,
+ messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
+ embedlyKey : process.env.REACT_APP_EMBEDLY_KEY
+}
+export default config;
